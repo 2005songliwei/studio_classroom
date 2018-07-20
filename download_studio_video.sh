07834ca23fe46f38fc5bb8ded1aae9b9980772e4
Liwei Song <liwei.song@windriver.com>
2018-07-20 16:56:22 +0800

    read video title name and error download file
    get video title as part of video name, add the a file use to store
    the failed download file.
    Add send email function to send email notification.
    
    Signed-off-by: Liwei Song <liwei.song@windriver.com>

--- download_studio_video.sh
+++ download_studio_video.sh
@@ -24,11 +24,12 @@ VIDEO_KEY=""
 VIDEO_SERIAL_NUM=""
 MP4_ADDR_URL_URL=""
 MP4_REAL_ADDR=""
-
+SC_DATE=""
+FILENAME_P1=""
+FILENAME_P2=""
 
 # REsolution can be "1080P 720P 480P 360P 270P 144P"
 RESOLUTION=""
-
 
 # temporary filename create by wget
 TMP_TV_PRO="/tmp/tv-programs.html"
@@ -40,7 +41,15 @@ SC_MP3="/tmp/SC`date "+%y%m%d"`.mp3"
 
 HOME_OF_MP3="$HOME/studio_classroom/SC_mp3"
 HOME_OF_MP4="$HOME/studio_classroom/SC_mp4"
+ERROR_DL_LIST="$HOME/studio_classroom/err_dl_list"
+EMAIL_CONTENT="/tmp/email_file"
 
+check_date(){
+	if [ `date "+%u"` == "7" ];then
+		echo "Error: There is no video on Sunday."
+		exit 1
+	fi
+}
 
 check_dir(){
 
@@ -56,6 +65,17 @@ check_dir(){
 
 }
 
+check_file(){
+	for file in $@
+	do
+		if [ ! -f $file ]; then
+			echo "INFO: Create file $file"
+			touch $ERROR_DL_LIST
+		else
+			echo "INFO: failed download file stored at: $ERROR_DL_LIST"
+		fi
+	done
+}
 
 clean_tmp_file(){
 	echo "INFO: remove tmeporary file: $TMP_TV_PRO $TMP_LINE_ME $MP4_URL_FILE"
@@ -74,6 +94,13 @@ read_resolution(){
 	fi
 }
 
+prepare_work(){
+	check_date
+	read_resolution
+	clean_tmp_file
+	check_dir $HOME_OF_MP3 $HOME_OF_MP4
+	check_file $ERROR_DL_LIST
+}
 
 # $1 is different VIDEO address, $2 is the output file create by wget.
 wget_html(){
@@ -84,8 +111,12 @@ wget_html(){
 	while [ $? -ne 0 ]
 	do
 		i=$((i+1))
-		if [ "$i" == 20 ];then
-			echo "ERROR: cat not connect $1, stopped"
+		if [ "$i" == 10 ];then
+			echo "ERROR: can not connect to $1, stopped"
+			if ! cat $ERROR_DL_LIST |grep `date "+%y%m%d"` &>/dev/null;then
+				echo "ERROR: SC`date "+%y%m%d"` download failed, stored in $ERROR_DL_LIST"
+				echo "SC`date "+%y%m%d"`" >> $ERROR_DL_LIST
+			fi
 			exit 1
 		fi
 		sleep 5
@@ -95,7 +126,7 @@ wget_html(){
 
 }
 
-prase_url(){
+parse_url(){
 
 	wget_html $VIDEO_URL $TMP_TV_PRO
 
@@ -118,10 +149,23 @@ prase_url(){
 	# Resolution(1080, 720P, 480P, 360P, 270P, 144P)
 	#MP4_REAL_ADDR=`sed "s/,/\n/g" $MP4_URL_FILE  |grep 1080P -A12 |grep mp4 |gawk -F"\"" '{print $4}'`
 	MP4_REAL_ADDR=`sed "s/,/\n/g" $MP4_URL_FILE  |grep -i $RESOLUTION -A12 |grep mp4 |gawk -F"\"" '{print $4}'`
-	echo "INFO: Successful got MP4 Address: $$MP4_REAL_ADDR"
+	echo "INFO: Successful got MP4 Address: $MP4_REAL_ADDR"
 
 	# download the video and store it as /tmp/SC.mp4
 	wget_html $MP4_REAL_ADDR $SC_MP4
+}
+
+# Record sc title to /home/studio_classroom/SC_mp3/SC_TITLE
+read_title_of_sc(){
+
+	SC_DATE=`grep -r -w "time" $TMP_TV_PRO  |awk -F">" '{print $2}' |awk -F"<" '{print $1}'`
+	month_day=`echo $SC_DATE |awk -F'/' '{print $2 $3}'`
+	FILENAME_P1=SC${SC_DATE:2:2}$month_day
+
+	tmp_title=`grep -r "sc-video-title sc-tvprograms-title" $TMP_TV_PRO -A1 |tail -1 |awk -F'<' '{print $1}'`
+	FILENAME_P2=`echo $tmp_title`
+	echo "INFO: Get video title: $FILENAME_P2"
+
 }
 
 convert_mp4_to_mp3(){
@@ -131,19 +175,39 @@ convert_mp4_to_mp3(){
 #store MP4 video if resolution is 1080P, or just store mp3 file only
 store_mp3_mp4(){
 
-	mv $SC_MP3 $HOME_OF_MP3/
+	read_title_of_sc
+
+	echo "INFO: move $SC_MP3 to $HOME_OF_MP3/"
+	mv $SC_MP3 $HOME_OF_MP3/"$FILENAME_P1 ($FILENAME_P2).mp3"
 
 	if [ "$RESOLUTION" == "1080P" ] || [ "$RESOLUTION" == "1080p" ];then
-		echo "INFO: copy $SC_MP4 to $HOME_OF_MP4"
-		mv $SC_MP4 $HOME_OF_MP4/
+		echo "INFO: move $SC_MP4 to $HOME_OF_MP4/"
+		mv $SC_MP4 $HOME_OF_MP4/"$FILENAME_P1 ($FILENAME_P2).mp4"
 	fi
 }
 
+send_email(){
+	rm $EMAIL_CONTENT -rf
+	echo "Subject: Studio Classroom download list -- `date "+%D"`" >> $EMAIL_CONTENT
+	echo >> $EMAIL_CONTENT
+	echo "Date: `date`" >> $EMAIL_CONTENT
+	echo >> $EMAIL_CONTENT
+	echo "mp3 list:" >> $EMAIL_CONTENT
+	echo "$HOME_OF_MP3/$FILENAME_P1 ($FILENAME_P2).mp3" >> $EMAIL_CONTENT
+	echo >> $EMAIL_CONTENT
+	echo >> $EMAIL_CONTENT
+	echo "Failed download list:" >> $EMAIL_CONTENT
+	cat $ERROR_DL_LIST >> $EMAIL_CONTENT
+	git send-email --to="liwei.song@windriver.com"  --thread --no-chain-reply-to $EMAIL_CONTENT
+}
 
-read_resolution
-clean_tmp_file
-check_dir $HOME_OF_MP3 $HOME_OF_MP4
-# REsolution can be "1080P 720P 480P 360P 270P 144P"
-prase_url
-convert_mp4_to_mp3
-store_mp3_mp4
+main_process(){
+	prepare_work
+	# REsolution can be "1080P 720P 480P 360P 270P 144P"
+	parse_url
+	convert_mp4_to_mp3
+	store_mp3_mp4
+	send_email
+}
+
+main_process
