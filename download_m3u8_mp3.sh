9727bb32b6c30bff9230d5162219d354f06cc45d
Liwei Song <liwei.song@windriver.com>
2018-08-20 11:21:30 +0800

    m3u8: add different audio type download support and error control
    Add "studio classroom" and "Let's talk in English" download support
    
    Add some error control when wget.
    
    Add send-email process to notice user.
    
    Signed-off-by: Liwei Song <liwei.song@windriver.com>

--- download_m3u8_mp3.sh
+++ download_m3u8_mp3.sh
@@ -5,21 +5,27 @@
 # It include three kinds of radio: "studio classroom", "Let's talk in English",
 # "Advanced".
 # 
-# This shell used to prase ".har" file stored through Firefox(press F12, then
-# play radio).
+# The main method to download audio is got m3u8 file from original website
+# http://m.studioclassroom.com/login_radio.php?radio=ad
 # 
-# We can get m3u8 file through .har file, then get the .ts and .key file
-# through m3u8 file, download .ts and .key file, then rewrite m3u8 file
-# make it can be used by ffmpeg.
+# After we got m3u8 file, from which we can also get the .ts and .key file
+# then rewrite m3u8 file to make it can be used by ffmpeg.
+#
+# There are three types of audio sc, ad, lt, we can provide at command line
+# For example: 
+#	download_m3u8_mp3.sh sc
+# this will start to download "studio classroom" audio.
+# by default, it will donwload "Advanced" audio.
 #
 # Author: LiweiSong <liwei.song@windriver.com>
 # 2018-2019 (c) LiweiSong - Wind River System, Inc.
 ###########################################################################
 
-AD_address="http://m.studioclassroom.com/login_radio.php?radio=ad"
-AD_html="AD_logined.html"
+login_html="logined.html"
 USERNAME=""
 PASSWORD=""
+#export AUDIO_address="http://m.studioclassroom.com/login_radio.php?radio=ad"
+export AUDIO_address
 
 export DATA_ACCOUNT
 export DATA_VIDEO_ID
@@ -36,12 +42,17 @@ export F_TS_LIST="ts_file_list"
 export sc_tmp_dir="/tmp/sc_download"
 export ts_dir=$sc_tmp_dir/ts-dir
 export key_dir=$sc_tmp_dir/key-dir
+export ERROR_DL_LIST=$sc_tmp_dir/err_dl_list
+export ERROR_DL_LOG=$sc_tmp_dir/err_dl_log
+export EMAIL_CONTENT=$sc_tmp_dir/email_file
 
-export sc_dir="$HOME/studio_classroom/SC_mp3"
+export sc_dir="$HOME/studio_classroom/SC_mp3_m3u8"
 export lt_dir="$HOME/studio_classroom/LT_mp3"
 export ad_dir="$HOME/studio_classroom/AD_mp3"
-export sc_file=""
-export ad_file="AD`date "+%y%m%d"`.mp3"
+#export sc_file="SC`date "+%y%m%d"`.MP3"
+#export mp3_filename="AD`date "+%y%m%d"`.mp3"
+export mp3_filename
+export mp3_dir
 
 export POLICYKEY
 export PARENT_m3u8_addr
@@ -49,6 +60,27 @@ export PART_OF_m3u8
 export LIST_OF_m3u8_addr
 export TS_ADDR_PRE
 
+
+check_video_type(){
+	if [ "$1" == "sc" ];then
+		AUDIO_address="http://m.studioclassroom.com/login_radio.php?radio=sc"
+		mp3_dir=$sc_dir
+		mp3_filename="SC`date "+%y%m%d"`.mp3"
+	elif [ "$1" == "ad" ];then
+		AUDIO_address="http://m.studioclassroom.com/login_radio.php?radio=ad"
+		mp3_filename="AD`date "+%y%m%d"`.mp3"
+		mp3_dir=$ad_dir
+	elif [ "$1" == "lt" ];then
+		AUDIO_address="http://m.studioclassroom.com/login_radio.php?radio=lt"
+		mp3_filename="LT`date "+%y%m%d"`.mp3"
+		mp3_dir=$lt_dir
+	else
+		AUDIO_address="http://m.studioclassroom.com/login_radio.php?radio=ad"	
+		mp3_filename="AD`date "+%y%m%d"`.mp3"
+		mp3_dir=$ad_dir
+	fi
+	echo "INFO: Audio file will be stored at: $mp3_dir/$mp3_filename"
+}
 
 check_tmp(){
 	if [ ! -d $sc_tmp_dir ];then
@@ -75,28 +107,59 @@ clean_dir(){
 	rm $sc_tmp_dir/* -rf
 }
 
+send_error_email(){
+        rm $EMAIL_CONTENT -rf
+        echo "Subject: [ERROR][m3u8][official website] Studio Classroom download list- -- `date "+%D"`" >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo "Date: `date`" >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo "Failed download list:" >> $EMAIL_CONTENT
+        echo "$mp3_dir/$mp3_filename" >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo "Failed log:" >> $EMAIL_CONTENT
+        cat $ERROR_DL_LOG >> $EMAIL_CONTENT
+        git send-email --to="liwei.song@windriver.com"  --thread --no-chain-reply-to --no-validate $EMAIL_CONTENT
+}
+
 get_m3u8_address(){
 	read -p "Input username: " USERNAME
 	read -s -p "Input password: " PASSWORD
 	echo
-	tsocks wget --quiet --post-data "username=$USERNAME&password=$PASSWORD" "$AD_address" -O $sc_tmp_dir/$AD_html
+	tsocks wget --tries=30 --post-data "username=$USERNAME&password=$PASSWORD" "$AUDIO_address" -O $sc_tmp_dir/$login_html 2>>$ERROR_DL_LOG
+	if [ $? -ne 0 ];then
+		echo "ERROR: failed to download $mp3_filename, wget $AUDIO_address error"
+		cat $ERROR_DL_LOG
+		send_error_email
+		exit 1
+	fi
 	# check data-account id to see if we login successful 
-	if grep -r "data-account" $sc_tmp_dir/$AD_html &>/dev/null; then
+	if grep -r "data-account" $sc_tmp_dir/$login_html &>/dev/null; then
 		echo "INFO: login successful."
 	else
 		echo "ERROR: login failed, Please check your username or password."
+		echo "ERROR: login failed, Please check your username or password." >>$ERROR_DL_LOG
+		send_error_email
 		exit 1
 	fi
-	DATA_ACCOUNT=`grep -r "data-account" $sc_tmp_dir/$AD_html|gawk -F"\"" '{print $2}'`
-	DATA_VIDEO_ID=`grep -r "data-video-id" $sc_tmp_dir/$AD_html|gawk -F"\"" '{print $2}'`
-	DATA_PLAYER=`grep -r "data-player" $sc_tmp_dir/$AD_html|gawk -F"\"" '{print $2}'`
-	DATA_EMBED=`grep -r "data-embed" $sc_tmp_dir/$AD_html|gawk -F"\"" '{print $2}'`
+	DATA_ACCOUNT=`grep -r "data-account" $sc_tmp_dir/$login_html|gawk -F"\"" '{print $2}'`
+	DATA_VIDEO_ID=`grep -r "data-video-id" $sc_tmp_dir/$login_html|gawk -F"\"" '{print $2}'`
+	DATA_PLAYER=`grep -r "data-player" $sc_tmp_dir/$login_html|gawk -F"\"" '{print $2}'`
+	DATA_EMBED=`grep -r "data-embed" $sc_tmp_dir/$login_html|gawk -F"\"" '{print $2}'`
 	
 	# this js include "policykey" which will be used when POST header to edge.api.brightcove.com
 	# index_min_js="http://players.brightcove.net/5210448787001/BJ9edqImx_default/index.min.js"
 	index_min_js="http://players.brightcove.net/${DATA_ACCOUNT}/${DATA_PLAYER}_${DATA_EMBED}/index.min.js"
 	echo INFO tsocks wget --quiet $index_min_js -O $sc_tmp_dir/$F_INDEX_JS
-	tsocks wget --quiet $index_min_js -O $sc_tmp_dir/$F_INDEX_JS
+	tsocks wget --tries=30 $index_min_js -O $sc_tmp_dir/$F_INDEX_JS 2>>$ERROR_DL_LOG
+	if [ $? -ne 0 ];then
+		echo "ERROR: failed to download $mp3_filename, wget $index_min_js error."
+		cat $ERROR_DL_LOG
+		send_error_email
+		exit 1
+	fi
 	sed -i "s/,/\n/g" $sc_tmp_dir/$F_INDEX_JS
 	POLICYKEY=`grep -r 'policyKey:"' $sc_tmp_dir/$F_INDEX_JS  |gawk -F"\"" '{print $2}'`
 
@@ -110,11 +173,24 @@ get_m3u8_address(){
 	echo "INFO: Original m3u8 stored at $sc_tmp_dir/$F_ORIGINAL_m3u8"
 	# run when curl version >= 7.55.0
 	#curl "$edge_address" -H @headerfile -o $sc_tmp_dir/$F_ORIGINAL_m3u8
+	if grep -r "http" $sc_tmp_dir/$F_ORIGINAL_m3u8 &>/dev/null; then
+		echo "INFO: get orginal m3u8 successful"
+	else
+		echo "ERROR: send curl command failed" >> $ERROR_DL_LOG
+		send_error_email
+		exit 1
+	fi
 	sed -i 's/"/\n/g' $sc_tmp_dir/$F_ORIGINAL_m3u8
 	PARENT_m3u8_addr=`cat $sc_tmp_dir/$F_ORIGINAL_m3u8 |grep "http:"`
 	echo "INFO: Parent m3u8 address is $PARENT_m3u8_addr"
 	echo INFO: tsocks wget --quiet $PARENT_m3u8_addr -O $sc_tmp_dir/$F_ADD_M3U8_LIST
-	tsocks wget --quiet $PARENT_m3u8_addr -O $sc_tmp_dir/$F_ADD_M3U8_LIST
+	tsocks wget --tries=30 $PARENT_m3u8_addr -O $sc_tmp_dir/$F_ADD_M3U8_LIST 2>>$ERROR_DL_LOG
+	if [ $? -ne 0 ];then
+		echo "ERROR: failed to download $mp3_filename, wget $PARENT_m3u8_addr error."
+		cat $ERROR_DL_LOG
+		send_error_email
+		exit 1
+	fi
 	
 	PART_OF_m3u8=`cat $sc_tmp_dir/$F_ADD_M3U8_LIST |grep "m3u8.hdntl"`
 	echo "INFO: The second part of m3u8 list address is: $PART_OF_m3u8"
@@ -122,7 +198,13 @@ get_m3u8_address(){
 	LIST_OF_m3u8_addr="http://hlstoken-a.akamaihd.net/${DATA_ACCOUNT}/$PART_OF_m3u8"
 	echo "INFO: List of m3u8 address is $LIST_OF_m3u8_addr"
 	echo INFO: tsocks wget -q "$LIST_OF_m3u8_addr" -O  $sc_tmp_dir/$F_TS_LIST
-	tsocks wget -q "$LIST_OF_m3u8_addr" -O  $sc_tmp_dir/$F_TS_LIST
+	tsocks wget --tries=30 "$LIST_OF_m3u8_addr" -O  $sc_tmp_dir/$F_TS_LIST 2>>$ERROR_DL_LOG
+	if [ $? -ne 0 ];then
+		echo "ERROR: failed to download $mp3_filename, wget $LIST_OF_m3u8_addr error."
+		cat $ERROR_DL_LOG
+		send_error_email
+		exit 1
+	fi
 	echo "INFO: ts file list is: $sc_tmp_dir/$F_TS_LIST"
 
 	TS_ADDR_PRE=http://hlstoken-a.akamaihd.net/${DATA_ACCOUNT}/`echo $PART_OF_m3u8 |gawk -F"/" '{print $1}'`
@@ -145,9 +227,11 @@ dl_ts(){
 	for ts_file in `cat $sc_tmp_dir/$F_TS_LIST |grep "\.ts"`
 	do
 		tmp_ts_name=`echo $ts_file |gawk -F"-" '{print $3}' |gawk -F"?" '{print $1}'`
-		tsocks wget --quiet -c $ts_file -O $ts_dir/$tmp_ts_name
+		tsocks wget --tries=30 -q -c $ts_file -O $ts_dir/$tmp_ts_name 2>>$ERROR_DL_LOG
 		if [ $? -ne 0 ];then
 			echo "Error: wget --quiet -c $ts_file -O $ts_dir/$tmp_ts_name"
+			echo "tsocks wget --tries=30 -q -c $ts_file -O $ts_dir/$tmp_ts_name" >>$ERROR_DL_LOG
+			send_error_email
 			exit 1
 		fi
 		echo -n "$tmp_ts_name "
@@ -169,9 +253,11 @@ dl_key(){
 	for key_file in `cat $sc_tmp_dir/$F_TS_LIST |grep "\.key" |gawk -F"\"" '{print $2}'`
 	do
 		tmp_key_name=`echo $key_file |gawk -F"-" '{print $4}' |gawk -F"?" '{print $1}'`
-		tsocks wget --quiet -c $key_file -O $key_dir/$tmp_key_name
+		tsocks wget --tries=30 -q -c $key_file -O $key_dir/$tmp_key_name 2>>$ERROR_DL_LOG
 		if [ $? -ne 0 ];then
 			echo "Error: wget --quiet -c $key_file -O $ts_dir/$tmp_key_name"
+			echo "Error: wget --quiet -c $key_file -O $ts_dir/$tmp_key_name" >>$ERROR_DL_LOG
+			send_error_email
 			exit 1
 		fi
 		echo -n "$tmp_key_name "
@@ -207,12 +293,38 @@ create_mp3(){
 	echo "INFO: Joint the ts file to mp3 with ffmpeg."
 	#ffmpeg -allowed_extensions ALL -i  $m3u8_file -c copy $sc_dir/$sc_file
 	#ffmpeg -allowed_extensions ALL -i  $m3u8_file -id3v2_version 3 $sc_dir/$sc_file
-	ffmpeg -allowed_extensions ALL -i  $sc_tmp_dir/$F_TS_LIST -map 0:a -b:a 128k  $ad_dir/$ad_file
+	/usr/local/bin/ffmpeg -allowed_extensions ALL -i  $sc_tmp_dir/$F_TS_LIST -map 0:a -b:a 128k  $mp3_dir/$mp3_filename
+	if [ $? -ne 0 ];then
+		echo "Error: ffmpeg -allowed_extensions ALL -i  $sc_tmp_dir/$F_TS_LIST -map 0:a -b:a 128k  $mp3_dir/$mp3_filename"
+	fi
 
-	echo "INFO: mp3 file stored at: $ad_dir/$ad_file"
+	echo "INFO: mp3 file stored at: $mp3_dir/$mp3_filename"
 }
 
+send_email(){
+        rm $EMAIL_CONTENT -rf
+        echo "Subject: [m3u8][$1][official website] Studio Classroom download list- -- `date "+%D"`" >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo "Date: `date`" >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo "File list:" >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo "$mp3_dir/$mp3_filename" >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo "Failed download list:" >> $EMAIL_CONTENT
+        cat $mp3_filename >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo >> $EMAIL_CONTENT
+        echo "Download log:" >> $EMAIL_CONTENT
+        cat $ERROR_DL_LOG >> $EMAIL_CONTENT
+        git send-email --to="liwei.song@windriver.com"  --thread --no-chain-reply-to --no-validate $EMAIL_CONTENT
+}
+
+
 main_process(){
+	check_video_type $@
 	check_tmp
 	clean_dir
 	get_m3u8_address
@@ -220,5 +332,6 @@ main_process(){
 	dl_key
 	create_m3u8_for_ffmpeg
 	create_mp3
+	send_email $@
 }
-main_process
+main_process $@
